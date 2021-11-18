f6da6441bacfb6dabf98947c9afcf4b4b20d7e2a
saisankp <prathameshsaisankar@Gmail.com>
2021-11-18 21:28:06 +0000

    Using original .jar with maven project

--- src/AccessGitHubAPI.java
+++ src/AccessGitHubAPI.java
@@ -1,41 +1,45 @@
-import org.kohsuke.github.*;
+
 import java.io.IOException;
+import java.util.Scanner;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class AccessGitHubAPI {
-    private final String TOKEN = "";
-    private GitHub gitHub;
-
-    public AccessGitHubAPI() throws IOException {
-        gitHub = new GitHubBuilder().withOAuthToken(TOKEN).build();
-    }
-
-    public static void main(String[] args) throws IOException {
-    	AccessGitHubAPI app = new AccessGitHubAPI();
-        app.printBanner();
-    }
-
-    private void printBanner(){
-        System.out.println("8888888b.                   .d888 d8b 888                                 \n" +
-                "888   Y88b                 d88P\"  Y8P 888                                 \n" +
-                "888    888                 888        888                                 \n" +
-                "888   d88P 888d888 .d88b.  888888 888 888  .d88b.                         \n" +
-                "8888888P\"  888P\"  d88\"\"88b 888    888 888 d8P  Y8b                        \n" +
-                "888        888    888  888 888    888 888 88888888                        \n" +
-                "888        888    Y88..88P 888    888 888 Y8b.                            \n" +
-                "888        888     \"Y88P\"  888    888 888  \"Y8888                         \n" +
-                "                                                                          \n" +
-                "                                                                          \n" +
-                "                                                                          \n" +
-                " .d8888b.                                                                 \n" +
-                "d88P  Y88b                                                                \n" +
-                "Y88b.                                                                     \n" +
-                " \"Y888b.   888  888 88888b.d88b.  88888b.d88b.   8888b.  888d888 888  888 \n" +
-                "    \"Y88b. 888  888 888 \"888 \"88b 888 \"888 \"88b     \"88b 888P\"   888  888 \n" +
-                "      \"888 888  888 888  888  888 888  888  888 .d888888 888     888  888 \n" +
-                "Y88b  d88P Y88b 888 888  888  888 888  888  888 888  888 888     Y88b 888 \n" +
-                " \"Y8888P\"   \"Y88888 888  888  888 888  888  888 \"Y888888 888      \"Y88888 \n" +
-                "                                                                      888 \n" +
-                "                                                                 Y8b d88P \n" +
-                "                                                                  \"Y88P\"  ");
-    }
+	public static void main(String[] args) {
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			try {
+				GitHubClient client = new GitHubClient();
+				System.out.println("Please enter your Github username");
+				String username = scanner.nextLine();
+				System.out.println("Welcome, " + username + "! Please enter your Github password");
+				String password = scanner.nextLine();
+				client.setCredentials(username, password);
+				RepositoryService repoService = new RepositoryService();
+				while (true) {
+					System.out.println(
+							"Please enter a username to view their repository information, or type 'quit' to exit.");
+					username = scanner.nextLine();
+					if (username.equalsIgnoreCase("quit"))
+						return;
+					for (Repository repo : repoService.getRepositories(username)) {
+						System.out.println("Repository name: " + repo.getName());
+						System.out.println("	- Language: " + repo.getLanguage());
+						System.out.println("	- Description: " + repo.getDescription());
+						System.out.println("	- Size: " + repo.getSize() + " kB");
+						System.out.println("	- Number of watchers: " + repo.getWatchers());
+						System.out.println("	- Number of forks: " + repo.getForks());
+						System.out.println("	- Created at: " + repo.getCreatedAt());
+						System.out.println("	- Updated at: " + repo.getUpdatedAt());
+						System.out.println("	- URL: " + repo.getGitUrl());
+					}
+				}
+			} catch (Exception e) {
+				System.out.println("Sorry, an unexpected error has occured.");
+			}
+			scanner.close();
+		}
+	}
 }
