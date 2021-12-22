10c68b402c48a5368c8f425b3632871da4302f2f
saisankp <prathameshsaisankar@Gmail.com>
2021-12-22 04:36:58 +0000

    Added final touches to scripting file

--- visualize.sh
+++ visualize.sh
@@ -1,3 +1,5 @@
+mvn clean install
+mvn exec:java -Dexec.mainClass="AccessGitHubAPI"
 cd visualization
 docker build -t vis .
 docker run --env-file <(bash -c 'env | grep AWS_') -it -p 8080-8100:8080-8100 vis
